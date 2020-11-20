80caf852b86ced4f6825490a2a291b119fb2b693
Nic Jackson <jackson.nic@gmail.com>
2020-11-20 16:02:51 +0000

    Updated configs

---
+++ scripts/setup_vault.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+# Enable auth
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault auth enable kubernetes'
+
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write auth/kubernetes/config \
+       token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+       kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443 \
+       kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
+
+# Enable database engine
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault secrets enable database'
+
+# Configure Vault access for the database
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write database/config/root \
+      plugin_name=postgresql-database-plugin \
+      allowed_roles="*" \
+      connection_url="postgresql://{{username}}:{{password}}@postgres:5432/wizard?sslmode=disable" \
+      username="root" \
+      password="please_change_me_barry"'
+
+# Force Vault to rotate the root password
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write --force /database/rotate-root/root'
+
+# Create the policy to read payments credentials
+cat <<EOF | vault policy write payments -
+path "database/creds/db-payments" {
+  capabilities = ["read"]
+}
+EOF
+
+# Create the policy to read search credentials
+cat <<EOF | vault policy write search -
+path "database/creds/db-search" {
+  capabilities = ["read"]
+}
+EOF
+
+# Create role for search
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write auth/kubernetes/role/search \
+      bound_service_account_names=search \
+      bound_service_account_namespaces=default \
+      policies=search \
+      ttl=1h'
+
+# Create role for payments
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write auth/kubernetes/role/payments \
+      bound_service_account_names=payments \
+      bound_service_account_namespaces=default \
+      policies= payments\
+      ttl=1h'
+
+# Configure limited creds for the search 
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write database/roles/db-search \
+      db_name=root \
+      creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD \"{{password}}\" VALID UNTIL \"{{expiration}}\"; \
+          GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
+      revocation_statements="ALTER ROLE \"{{name}}\" NOLOGIN;"\
+      default_ttl="1h" \
+      max_ttl="24h"'
+
+# Configure limited creds for payments
+kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
+  sh -c ' \
+    vault write database/roles/db-payments \
+      db_name=root \
+      creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD \"{{password}}\" VALID UNTIL \"{{expiration}}\"; \
+          GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
+      revocation_statements="ALTER ROLE \"{{name}}\" NOLOGIN;"\
+      default_ttl="1h" \
+      max_ttl="24h"'
