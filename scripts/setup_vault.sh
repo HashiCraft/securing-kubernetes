e67daac5b74876037ae9c01c1e20d7d5bda555a0
Nic Jackson <jackson.nic@gmail.com>
2020-11-23 12:45:21 +0000

    Updated config

--- scripts/setup_vault.sh
+++ scripts/setup_vault.sh
@@ -2,30 +2,27 @@
 
 # Enable auth
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
-    vault auth enable kubernetes'
+    vault auth enable kubernetes
 
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
+  sh -c '
     vault write auth/kubernetes/config \
        token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-       kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443 \
+       kubernetes_host=https://${KUBERNETES_SERVICE_HOST}:443 \
        kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
 
 # Enable database engine
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
-    vault secrets enable database'
+    vault secrets enable database
 
 # Configure Vault access for the database
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
     vault write database/config/root \
       plugin_name=postgresql-database-plugin \
       allowed_roles="*" \
-      connection_url="postgresql://{{username}}:{{password}}@postgres:5432/wizard?sslmode=disable" \
+      connection_url="postgresql://{{username}}:{{password}}@database:5432/root?sslmode=disable" \
       username="root" \
-      password="please_change_me_barry"'
+      password="please_change_me_barry"
 
 # Force Vault to rotate the root password
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
@@ -46,42 +43,38 @@ path "database/creds/db-search" {
 }
 EOF
 
-# Create role for search
+# Create k8s auth role for search
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
     vault write auth/kubernetes/role/search \
       bound_service_account_names=search \
       bound_service_account_namespaces=default \
       policies=search \
-      ttl=1h'
+      ttl=1h
 
-# Create role for payments
+# Create k8s auth role for payments
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
     vault write auth/kubernetes/role/payments \
       bound_service_account_names=payments \
       bound_service_account_namespaces=default \
-      policies= payments\
-      ttl=1h'
+      policies=payments\
+      ttl=1h
 
-# Configure limited creds for the search 
+# Configure db auth limited creds for the search 
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
     vault write database/roles/db-search \
       db_name=root \
-      creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD \"{{password}}\" VALID UNTIL \"{{expiration}}\"; \
-          GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
+      creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
+          GRANT SELECT ON branches TO \"{{name}}\";" \
       revocation_statements="ALTER ROLE \"{{name}}\" NOLOGIN;"\
       default_ttl="1h" \
-      max_ttl="24h"'
+      max_ttl="24h"
 
-# Configure limited creds for payments
+# Configure db auth limited creds for payments
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
-  sh -c ' \
     vault write database/roles/db-payments \
       db_name=root \
-      creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD \"{{password}}\" VALID UNTIL \"{{expiration}}\"; \
+      creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
           GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
       revocation_statements="ALTER ROLE \"{{name}}\" NOLOGIN;"\
       default_ttl="1h" \
-      max_ttl="24h"'
+      max_ttl="24h"
